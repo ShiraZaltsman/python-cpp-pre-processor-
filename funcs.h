59c39b6d0a89d7cec99ebf290d707838ce72ae59
shira zaltsman <shira.zaltsman@gmail.com>
2019-09-18 23:17:16 +0300

    need a littel fix

---
+++ funcs.h
@@ -0,0 +1,20 @@
+#ifndef __FUNCS__H
+#define __FUNCS__H
+
+#include "dummy.h"
+
+enum E_STYLE
+{
+	E_START,
+	E_END
+};
+
+int		get_num();
+int		get_max(int num1, int num2);
+int		get_min(int num1, int num2);
+int	    get_sum(int num1, int num2);
+int 	factorial(int n);
+void 	print_current_time();
+void	decorate(E_STYLE style);
+
+#endif	//__FUNCS__H
